f06476b9d18de4608e61bfc80943f4a5082e519c
xiaolu <Saladbob@outlook.com>
2022-12-15 22:48:18 +0800

    分库分表测试完成

--- JavaProject/db-router-spring-boot-starter/src/main/java/cn/bugstack/middleware/db/router/annotation/DBRouter.java
+++ JavaProject/db-router-spring-boot-starter/src/main/java/cn/bugstack/middleware/db/router/annotation/DBRouter.java
@@ -4,11 +4,10 @@ import java.lang.annotation.*;
 
 
 /**
- * @description: 路由注解
- * @author: 小傅哥，微信：fustack
- * @date: 2021/9/22
- * @github: https://github.com/fuzhengwei
- * @Copyright: 公众号：bugstack虫洞栈 | 博客：https://bugstack.cn - 沉淀、分享、成长，让自己和他人都能有所收获！
+ * 路由注解
+ *
+ * @author xiaolu
+ * @since 2022-12-15
  */
 @Documented
 @Retention(RetentionPolicy.RUNTIME)
