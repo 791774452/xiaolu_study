277d4f3aa077b68d2101bb897c175ab42fecf547
xiaolu <Saladbob@outlook.com>
2022-12-15 22:15:58 +0800

    分库分表

---
+++ JavaProject/Lottery/lottery-infrastructure/src/main/java/cn/itedus/lottery/infrastructure/dao/IUserTakeActivityDao.java
@@ -0,0 +1,24 @@
+package cn.itedus.lottery.infrastructure.dao;
+
+import cn.bugstack.middleware.db.router.annotation.DBRouter;
+import cn.itedus.lottery.infrastructure.po.UserTakeActivity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * 用户领取活动表DAO
+ *
+ * @author xiaolu
+ * @since 2022-12-15
+ */
+@Mapper
+public interface IUserTakeActivityDao {
+
+    /**
+     * 插入用户领取活动信息
+     *
+     * @param userTakeActivity 入参
+     */
+    @DBRouter(key = "uId")
+    void insert(UserTakeActivity userTakeActivity);
+
+}
